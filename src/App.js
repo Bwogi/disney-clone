30561e627622d8dbf0f9894f9da9c5cc7c5c9466
Andrew Bwogi <andrew.bwogi@gmail.com>
2021-04-24 06:26:05 -0700

    Login styled component add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Login from "./components/Login";
 import './App.css';
 
 function App() {
-    return ( <
-        div className = "App" >
-          <p>Disney clone</p>
-        <
-        /div>
+    return ( 
+      <div className = "App" >
+            <Router>
+                <Switch>
+                  <Route exact path="/">
+                    <Login />
+                  </Route>
+                </Switch>
+            </Router>        
+        </div>
     );
 }
 
---
+++ src/components/Login.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+const Login = (props) => {
+    return (
+        <Container>
+            <Content>
+                Content
+            </Content>
+        </Container>
+    )
+};
+
+const Container = styled.section`
+overflow: hidden;
+display: flex;
+flex-direction: column;
+text-align: center;
+height: 100vh;
+`;
+const Content = styled.div``;
+
+export default Login;
